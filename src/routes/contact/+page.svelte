a67180a0b0649ba5cf6b83db3ef636dd67a39e27
Lewis Torrington <lewis.torrington@upcloud.com>
2023-09-03 21:59:31 +0300

    feat: contact barebones

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,17 +1,54 @@
 <script>
-
   import GlassCard from "../../components/GlassCard.svelte";
   import SwipeWrapper from "../../components/SwipeWrapper.svelte";
 
   let leftLink = "/data";
   let rightLink = "/";
+
+  let sender_address = '';
+  let subject = '';
+  let content = '';
+  let responseMessage = '';
+
+  async function handleSubmit() {
+    const response = await fetch('/contact', {
+      method: 'POST',
+      body: JSON.stringify({ sender_address, subject, content }),
+      headers: {
+        'content-type': 'application/json'
+      }
+    });
+
+    responseMessage = await response.json();
+  }
 </script>
 
 <SwipeWrapper leftLink={leftLink} rightLink={rightLink}>
   <div style="padding: 1rem;">
     <h1>Contact</h1>
 
-    <!-- TODO -->
+    <form on:submit={handleSubmit}>
+      <label for="sender_address">Sender Address:</label>
+      <input type="email" id="sender_address" bind:value={sender_address} required />
+
+      <br />
+
+      <label for="subject">Subject:</label>
+      <input type="text" id="subject" bind:value={subject} required />
+
+      <br />
+
+      <label for="content">Content:</label>
+      <textarea id="content" bind:value={content} required />
+
+      <br />
+
+      <button type="submit">Send Message</button>
+    </form>
+  
+    {#if responseMessage}
+      <p>{responseMessage}</p>
+    {/if}
   </div>
   <GlassCard style="margin-left: 1rem;">
     <p class="no-padding">
---
+++ src/routes/contact/+server.js
@@ -0,0 +1,31 @@
+import { json } from '@sveltejs/kit';
+import { LIVE_EXPLAN_API_URL, LIVE_EXPLAN_API_TOKEN } from '$env/static/private'
+ 
+/** @type {import('./$types').RequestHandler} */
+export async function POST({ request }) {
+  const { sender_address, subject, content } = await request.json();
+  const data = { sender_address, subject, content }
+
+  let responseMessage = ''
+
+  try {
+    const response = await fetch(`${LIVE_EXPLAN_API_URL}/contact`, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${LIVE_EXPLAN_API_TOKEN}`,
+      },
+      body: JSON.stringify(data),
+    })
+
+    if (response.ok) {
+      responseMessage = 'Message sent successfully!'
+    } else {
+      let wholeResponse = await response.json()
+      responseMessage = wholeResponse.error
+    }
+  } catch (error) {
+    // @ts-ignore
+    responseMessage = error.message
+  }
+  return json(responseMessage)
+}

776fbe834087a981fda243b7e6ee18afa63784d6
Lewis Torrington <lewis.torrington@upcloud.com>
2023-09-04 20:36:41 +0300

    fix: better url and ip

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -15,14 +15,14 @@ export async function handle({ event, resolve }) {
       body: JSON.stringify({
         "for_user": 1, // me
         "visited_at": new Date().toISOString(),
-        "url_path": event.url.href,
-        "ip_address": event.getClientAddress(),
-        "geolocation": null, //TODO
-        "ip_isp": null, //TODO
+        "url_path": event.request.headers.get("X-Original-URL"),
+        "ip_address": event.request.headers.get("X-Real-IP"),
+        "geolocation": null, // TODO
+        "ip_isp": null, // TODO
         "browser": event.request.headers.get("User-Agent"),
         "operating_system": event.request.headers.get("Sec-Ch-Ua-Platform"),
         "is_mobile": event.request.headers.get("Sec-Ch-Ua-Mobile") == "?1",
-        "referer_url": event.request.referrer,
+        "referer_url": null, // TODO
         "preferred_languages": event.request.headers.get("Accept-Language"),
         "cookies": event.cookies.getAll().map(cookie => `${cookie.name}=${cookie.value}`).join(','),
         "body": event.request.body,

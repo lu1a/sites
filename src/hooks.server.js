f20a53ac009171dc05b5fcc061380dbed3c0afff
Lewis Torrington <lewis.torrington@upcloud.com>
2023-09-09 13:35:05 +0300

    fix: new fields to visitor log

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -5,13 +5,36 @@ export async function handle({ event, resolve }) {
   const editableRequest = event.request.clone()
   let bodyString = await editableRequest.text()
   bodyString = bodyString.replace(/{/g, "[[").replace(/}/g, "]]")
+
+  let ip = event.request.headers.get("X-Real-IP") || ''
+  let ipData = {
+    "isp": null,
+    "country": null,
+    "city": null,
+    "zip": null,
+    "lat": null,
+    "lon": null,
+  }
+  try {
+    let ipDataResponse = await fetch(`http://ip-api.com/json/${ip}`, {
+      method: 'GET',
+    })
+    ipData = await ipDataResponse.json()
+  } catch (e) {
+    console.log("The IP API I'm using failed: ", e)
+  }
+
   sendVisitorLog({
     "for_user": 1, // me
     "visited_at": new Date().toISOString(),
     "url_path": event.request.headers.get("X-Original-URL"),
-    "ip_address": event.request.headers.get("X-Real-IP"),
-    "geolocation": null, // TODO
-    "ip_isp": null, // TODO
+    "ip_address": ip,
+    "ip_isp": ipData.isp,
+    "ip_country": ipData.country,
+    "ip_city": ipData.city,
+    "ip_zip": ipData.zip,
+    "ip_latitude": ipData.lat,
+    "ip_longitude": ipData.lon,
     "browser": event.request.headers.get("User-Agent"),
     "operating_system": event.request.headers.get("Sec-Ch-Ua-Platform"),
     "is_mobile": event.request.headers.get("Sec-Ch-Ua-Mobile") == "?1",

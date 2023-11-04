518c8cce148d3ba44ae44bcb0ca941ee7b46d864
lewistorrington <lewis.torrington@upcloud.com>
2023-11-04 18:14:49 +0200

    feat: no navigation necessary

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,11 @@
 <script>
-	import BottomNav from "../lib/BottomNav.svelte";
 	import GlassCard from "../lib/GlassCard.svelte";
+	import GreyCard from "../lib/GreyCard.svelte";
 
-    let rightLink = "/about";
 </script>
 
 <svelte:head>
-  <title>Lewis' portfolio site: home</title> 
+  <title>Lewis' portfolio site</title> 
 </svelte:head>
 
 <main class="overflow-hidden mx-auto p-8 max-w-xl">
@@ -36,9 +35,135 @@
         tell me that almost every non-bot who visited here 
         has been on a phone!
     </p>
-  </article>
 
+    <br /><br /><br />
+
+    <h2 class="text-3xl">About me</h2>
+    <br />
+    <GreyCard>
+      <ul class="list-none">
+        <li>From Australia; living in Finland since 2019.</li>
+        <li>
+          Working at&nbsp;
+          <a
+            class="underline"
+            href="https://www.upcloud.com/"
+            target="_blank"
+            >UpCloud</a>.
+        </li>
+        <li>Software engineer:</li>
+        <ul class="list-disc ml-4">
+          <li>Specialties: cloud backend, data engineering</li>
+          <li>Best langs right now: Go, Python, JS/TS</li>
+          <li>Getting better at: data science, k8s</li>
+        </ul>
+        <li class="mt-2">
+        BCompSc (m.: Cyber Security),&nbsp;
+        <a
+          class="underline"
+          href="https://www.uow.edu.au/"
+          target="_blank"
+          >UoW</a>, 2016-2018.
+        </li>
+      </ul>
+    </GreyCard>
+    <br />
+    <GreyCard>
+      <p>
+        Instead of a CV, just go look at my&nbsp;
+        <a
+          class="underline"
+          href="https://www.linkedin.com/in/lewis-torrington/"
+          target="_blank"
+        >
+          LinkedIn</a>. 😇
+        <br />
+        Ping me if you want me to contribute to your git project!
+        (<a
+          class="underline"
+          href="https://github.com/lu1a"
+          target="_blank"
+        >
+          My GitHub</a>.)
+      </p>
+    </GreyCard>
+
+    <br /><br /><br />
+
+    <h2 class="text-3xl">Some projects</h2>
+    <br />
+    <GreyCard>
+      <p class="m-0">
+        <a
+          class="underline"
+          href="https://github.com/lu1a/cosmas-api"
+          target="_blank"
+        >
+          COSMAS
+        </a>
+        <br />
+        An API in Rust to allow for an automated social peer-to-peer sharing system.
+        <br />
+        Designed for my future farm in mind. 🧑‍🌾🌾
+      </p>
+    </GreyCard>
+    <br />
+
+    <GreyCard>
+      <p class="m-0">
+        <a
+          class="underline"
+          href="https://github.com/lu1a/kasu"
+          target="_blank"
+        >
+          KASU
+        </a>
+        <br />
+        A little script for installing Kubernetes on a blank Ubuntu machine!
+        <br />
+        Either for becoming a control plane or worker node.
+      </p>
+    </GreyCard>
+    <br />
+
+    <p class="m-0 text-gray-400 text-lg">
+      <i>...and more! Though not that much more that I wanna show publicly.
+      Check out my GitHub anyhow if you&apos;re interested.</i></p>
+
+    <br /><br /><br />
+
+    <h2 class="text-3xl">Data</h2>
+    <br />
+    <GreyCard>
+      <p class="m-0">This site was written in Svelte by me.<br />You can find the&nbsp;
+        <a
+          class="underline"
+          href="https://github.com/lu1a/portfolio-site"
+          target="_blank"
+        >
+          source code
+        </a>
+        &nbsp;on my GitHub.<br />As you&apos;ll be able to see there, I have no code for
+        cookies or nasty things - though I do log my visitors to a TimeScale instance for fun.
+        The only fruitful thing that&apos;s gotten me so far is the knowledge of how many
+        US-based bots there are out there.
+      </p>
+    </GreyCard>
+
+    <br />
+
+    <p class="m-0 text-gray-400 text-lg">
+      <i>// TODO: link here to a beautiful grafana page about my website stats!</i></p>
+
+
+    <div class="flex justify-center mt-48">
+      <a href="https://www.linkedin.com/in/lewis-torrington/">
+        <button class="animate-ping absolute inline-flex w-52 h-16 rounded-full bg-blue-600 opacity-60" aria-hidden="true"></button>
+        <button class="bg-blue-700 w-52 h-16 text-blue-300 rounded-md font-semibold text-2xl font-sans">
+          <span>Get in touch</span>
+        </button>
+      </a>
+    </div>
+
+  </article>
 </main>
-<footer>
-  <BottomNav leftLink={null} rightLink={rightLink} />
-</footer>

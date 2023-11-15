98d7748e84aa0d60b698d9647aa9259e6959c3ad
lewistorrington <lewis.torrington@upcloud.com>
2023-11-15 14:49:48 +0200

    fix: don't do godotenv

--- main.go
+++ main.go
@@ -7,17 +7,11 @@ import (
 	"time"
 
 	"github.com/charmbracelet/log"
-	"github.com/joho/godotenv"
 	"github.com/lu1a/portfolio-site/service"
 	"github.com/lu1a/portfolio-site/types"
 )
 
 func main() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Did you create and fill a '.env' file?", "err", err)
-	}
-
 	shutdownTimeout, err := time.ParseDuration(os.Getenv("SHUTDOWN_TIMEOUT"))
 	if err != nil {
 		log.Fatal("Pls set the shutdown timeout correctly", "err", err)

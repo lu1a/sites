46a4e8fb03c79398a35f0c076cd3ccd433beaeeb
lewistorrington <lewis.torrington@upcloud.com>
2024-04-27 13:57:08 +0300

    fix: rust build then rename

--- ci-build-deploy.sh
+++ ci-build-deploy.sh
@@ -30,7 +30,8 @@ build_stage() {
     echo "Running build stage"
 
     # Add any custom actions you want to perform when a new change is detected
-    cargo build --release --bin portfolio-site -o portfolio-site-$latest_commit
+    cargo build --release --bin portfolio-site
+    mv /targets/release/portfolio-site /targets/release/portfolio-site-$latest_commit
 }
 
 deploy_stage() {

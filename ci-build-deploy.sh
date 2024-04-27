0a7cb830e073c40b92a8a4d926b3020f0cc3febf
lewistorrington <lewis.torrington@upcloud.com>
2024-04-27 14:25:08 +0300

    fix: let cron or systemd timers handle the reccurance

--- ci-build-deploy.sh
+++ ci-build-deploy.sh
@@ -5,25 +5,44 @@ USERNAME="lu1a"
 REPO_NAME="portfolio-site"
 
 # URL of the GitHub repository's main branch
-URL="https://github.com/${USERNAME}/${REPO_NAME}/commits/main"
+GITHUB_URL="https://github.com/${USERNAME}/${REPO_NAME}/commits/main"
+
+RELEASE_FOLDER="./target/release"
 
 last_commit=""
 latest_commit=""
 
-# TODO: check if the latest commit has a change to this script, and reload self if so
+get_last_deployed_commit() {
+    file_path=$(find $RELEASE_FOLDER -type f -name 'portfolio-site-*' -print -quit)
 
-check_for_new_commit() {
-    # Use curl to fetch the HTML content of the repository's main branch page
-    html_content=$(curl -s "$URL")
-
-    # Check if the request was successful
-    if [ $? -ne 0 ]; then
-        echo "Error: Failed to fetch HTML content from GitHub"
-        exit 1
+    if [[ -n "$file_path" ]]; then
+        # Extract the filename from the path
+        filename=$(basename "$file_path")
+        
+        # Extract the latter half of the filename
+        last_commit="${filename##*-}"
+        
+        # Now you can use $latter_half variable as needed
+        echo "Last deployed commit: $last_commit"
+    else
+        echo "No last deployed commit file found."
+        # exit 1
     fi
+}
+
+get_latest_commit() {
+    # Use curl to fetch the HTML content of the repository's main branch page
+    html_content=$(curl -s "$GITHUB_URL")
 
     # Extract the latest commit SHA from the HTML content (using a simple pattern match)
     latest_commit=$(echo "$html_content" | grep -oE 'commit\/[a-f0-9]{40}' | head -n1 | cut -d'/' -f2)
+
+    # Check if the request was successful
+    if [[ -z "$latest_commit" ]]; then
+        echo "Error: Failed to fetch HTML content from GitHub"
+        exit 1
+    fi
+    echo "Latest commit on GitHub: $latest_commit"
 }
 
 build_stage() {
@@ -31,38 +50,26 @@ build_stage() {
 
     # Add any custom actions you want to perform when a new change is detected
     cargo build --release --bin portfolio-site
-    mv ./target/release/portfolio-site ./target/release/portfolio-site-$latest_commit
+    mv $RELEASE_FOLDER/portfolio-site $RELEASE_FOLDER/portfolio-site-$latest_commit
+    chmod 700 $RELEASE_FOLDER/portfolio-site-$latest_commit
+
+    rm $RELEASE_FOLDER/portfolio-site-$last_commit
 }
 
 deploy_stage() {
     echo "Running deploy stage"
     # Add any custom actions you want to perform when a new change is detected
+
 }
 
-# Initialize the last_commit variable
-html_content=$(curl -s "$URL")
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to fetch HTML content from GitHub"
-    exit 1
+get_last_deployed_commit
+get_latest_commit
+
+if [[ "$latest_commit" != "$last_commit" ]]; then
+    echo "A new change has been pushed"
+
+    build_stage
+    deploy_stage
+
+    last_commit="$latest_commit"
 fi
-last_commit=$(echo "$html_content" | grep -oE 'commit\/[a-f0-9]{40}' | head -n1 | cut -d'/' -f2)
-
-# Infinite loop
-while true
-do
-    check_for_new_commit
-
-    # Check if the latest commit SHA has changed (indicating a new push)
-    if [[ "$latest_commit" != "$last_commit" ]]; then
-        echo "A new change has been pushed"
-        last_commit="$latest_commit"
-
-        build_stage
-        deploy_stage
-    else
-        echo "Nothing new"
-    fi
-
-    # Wait for 10 seconds before the next check
-    sleep 10
-done

a20a81db2495a8b9691e1601c742723c9d9da209
lewistorrington <lewis.torrington@upcloud.com>
2024-04-27 14:31:39 +0300

    fix: nicer var names

--- ci-build-deploy.sh
+++ ci-build-deploy.sh
@@ -9,8 +9,8 @@ GITHUB_URL="https://github.com/${USERNAME}/${REPO_NAME}/commits/main"
 
 RELEASE_FOLDER="./target/release"
 
-last_commit=""
-latest_commit=""
+LAST_DEPLOYED_COMMIT=""
+LATEST_COMMIT=""
 
 get_last_deployed_commit() {
     file_path=$(find $RELEASE_FOLDER -type f -name 'portfolio-site-*' -print -quit)
@@ -20,12 +20,11 @@ get_last_deployed_commit() {
         filename=$(basename "$file_path")
         
         # Extract the latter half of the filename
-        last_commit="${filename##*-}"
+        LAST_DEPLOYED_COMMIT="${filename##*-}"
         
-        # Now you can use $latter_half variable as needed
-        echo "Last deployed commit: $last_commit"
+        echo "Last deployed commit: $LAST_DEPLOYED_COMMIT"
     else
-        echo "No last deployed commit file found."
+        echo "No previous deployment found."
     fi
 }
 
@@ -34,38 +33,36 @@ get_latest_commit() {
     html_content=$(curl -s "$GITHUB_URL")
 
     # Extract the latest commit SHA from the HTML content (using a simple pattern match)
-    latest_commit=$(echo "$html_content" | grep -oE 'commit\/[a-f0-9]{40}' | head -n1 | cut -d'/' -f2)
+    LATEST_COMMIT=$(echo "$html_content" | grep -oE 'commit\/[a-f0-9]{40}' | head -n1 | cut -d'/' -f2)
 
     # Check if the request was successful
-    if [[ -z "$latest_commit" ]]; then
+    if [[ -z "$LATEST_COMMIT" ]]; then
         echo "Error: Failed to fetch HTML content from GitHub"
         exit 1
     fi
-    echo "Latest commit on GitHub: $latest_commit"
+    echo "Latest commit on GitHub: $LATEST_COMMIT"
 }
 
 build_stage() {
     echo "Running build stage"
 
-    # Add any custom actions you want to perform when a new change is detected
     cargo build --release --bin portfolio-site
-    mv $RELEASE_FOLDER/portfolio-site $RELEASE_FOLDER/portfolio-site-$latest_commit
-    chmod 700 $RELEASE_FOLDER/portfolio-site-$latest_commit
+    mv $RELEASE_FOLDER/portfolio-site $RELEASE_FOLDER/portfolio-site-$LATEST_COMMIT
+    chmod 700 $RELEASE_FOLDER/portfolio-site-$LATEST_COMMIT
 }
 
 deploy_stage() {
     echo "Running deploy stage"
-    # Add any custom actions you want to perform when a new change is detected
 
     # TODO: actual systemd switchover
 
-    rm $RELEASE_FOLDER/portfolio-site-$last_commit
+    rm $RELEASE_FOLDER/portfolio-site-$LAST_DEPLOYED_COMMIT
 }
 
 get_last_deployed_commit
 get_latest_commit
 
-if [[ "$latest_commit" != "$last_commit" ]]; then
+if [[ "$LATEST_COMMIT" != "$LAST_DEPLOYED_COMMIT" ]]; then
     echo "A new change has been pushed"
 
     build_stage

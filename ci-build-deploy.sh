3c3f9002c7faaf8d2d4ebe4bd1dadd41566ed5c2
lewistorrington <lewis.torrington@upcloud.com>
2024-04-28 12:19:16 +0300

    feat: slightly more robust CI

--- ci-build-deploy.sh
+++ ci-build-deploy.sh
@@ -7,14 +7,18 @@ REPO_NAME="portfolio-site"
 # URL of the GitHub repository's main branch
 GITHUB_URL="https://github.com/${USERNAME}/${REPO_NAME}/commits/main"
 
-REPO_FOLDER="/root/Repositories/portfolio-site"
+REPO_FOLDER="/root/Repositories/${REPO_NAME}"
 RELEASE_FOLDER="${REPO_FOLDER}/target/release"
+
+NGINX_CONFIG_FILE="/etc/nginx/sites-available/lewistorrington.fi"
+DESIRED_PORT=3000
+SYSTEMD_CONFIG_FOLDER="/etc/systemd/system"
 
 LAST_DEPLOYED_COMMIT=""
 LATEST_COMMIT=""
 
 get_last_deployed_commit() {
-    file_path=$(find $RELEASE_FOLDER -type f -name 'portfolio-site-*' -print -quit)
+    file_path=$(find $RELEASE_FOLDER -type f -name '$REPO_NAME-*' -print -quit)
 
     if [[ -n "$file_path" ]]; then
         # Extract the filename from the path
@@ -30,28 +34,15 @@ get_last_deployed_commit() {
 }
 
 get_latest_commit() {
-    # Use curl to fetch the HTML content of the repository's main branch page
-    html_content=$(curl -s "$GITHUB_URL")
-
-    # Extract the latest commit SHA from the HTML content (using a simple pattern match)
-    LATEST_COMMIT=$(echo "$html_content" | grep -oE 'commit\/[a-f0-9]{40}' | head -n1 | cut -d'/' -f2)
-
-    # Check if the request was successful
-    if [[ -z "$LATEST_COMMIT" ]]; then
-        echo "Error: Failed to fetch HTML content from GitHub"
-        exit 1
-    fi
-    echo "Latest commit on GitHub: $LATEST_COMMIT"
-}
-
-pull_latest_commit() {
     git -C $REPO_FOLDER pull
+    LATEST_COMMIT=$(git -C $REPO_FOLDER rev-parse HEAD)
+    echo "Latest commit on GitHub: $LATEST_COMMIT"
 }
 
 build_stage() {
     echo "Running build stage"
 
-    cargo build --manifest-path=/root/Repositories/portfolio-site/Cargo.toml --release --bin portfolio-site
+    cargo build --manifest-path=$REPO_FOLDER/Cargo.toml --release --bin $REPO_NAME
     mv $RELEASE_FOLDER/portfolio-site $RELEASE_FOLDER/portfolio-site-$LATEST_COMMIT
     chmod 700 $RELEASE_FOLDER/portfolio-site-$LATEST_COMMIT
 }
@@ -59,14 +50,13 @@ build_stage() {
 deploy_stage() {
     echo "Running deploy stage"
 
-    nginx_config_file="/etc/nginx/sites-available/lewistorrington.fi"
     # Define the ports to toggle between
-    port1="3000"
-    port2="3001"
-    port3="3002"
-    new_port="3000"
+    port1="$((DESIRED_PORT))"
+    port2="$((DESIRED_PORT + 1))"
+    port3="$((DESIRED_PORT + 2))"
+    new_port="$((DESIRED_PORT))"
     # Use grep and awk to find the proxy_pass directive and extract the port number
-    proxy_pass_port=$(grep -E '^\s*proxy_pass\s+http://127.0.0.1:([0-9]+);' "$nginx_config_file" | awk -F':' '{print $NF}')
+    proxy_pass_port=$(grep -E '^\s*proxy_pass\s+http://127.0.0.1:([0-9]+);' "$NGINX_CONFIG_FILE" | awk -F':' '{print $NF}')
     proxy_pass_port="${proxy_pass_port%;}"
     # Check the extracted port number and toggle between port1 and port2
     if [ "$proxy_pass_port" == "$port1" ]; then
@@ -88,7 +78,7 @@ deploy_stage() {
     # Create and run a new service for the newly built commit
     new_service_contents=$(cat <<EOF
 [Unit]
-Description=portfolio-site service
+Description=$REPO_NAME service
 After=network.target
 StartLimitIntervalSec=0
 [Service]
@@ -96,27 +86,27 @@ Type=simple
 Restart=always
 RestartSec=1
 User=root
-ExecStart=$RELEASE_FOLDER/portfolio-site-$LATEST_COMMIT $new_port $REPO_FOLDER/static
+ExecStart=$RELEASE_FOLDER/$REPO_NAME-$LATEST_COMMIT $new_port $REPO_FOLDER/static
 
 [Install]
 WantedBy=multi-user.target
 EOF
     )
-    echo "$new_service_contents" > /etc/systemd/system/portfolio-site-$LATEST_COMMIT.service
-    systemctl start portfolio-site-$LATEST_COMMIT
-    systemctl enable portfolio-site-$LATEST_COMMIT
+    echo "$new_service_contents" > $SYSTEMD_CONFIG_FOLDER/$REPO_NAME-$LATEST_COMMIT.service
+    systemctl start $REPO_NAME-$LATEST_COMMIT
+    systemctl enable $REPO_NAME-$LATEST_COMMIT
 
     # Wait 5 seconds for the new service to properly start up
     sleep 5
 
-    sed -i "s|^\([[:space:]]*proxy_pass[[:space:]]\+http://127.0.0.1:\)[0-9]\+;|\1$new_port;|" "$nginx_config_file"
+    sed -i "s|^\([[:space:]]*proxy_pass[[:space:]]\+http://127.0.0.1:\)[0-9]\+;|\1$new_port;|" "$NGINX_CONFIG_FILE"
     nginx -t && nginx -s reload
 
-    systemctl disable portfolio-site-$LAST_DEPLOYED_COMMIT
-    systemctl stop portfolio-site-$LAST_DEPLOYED_COMMIT
-    rm /etc/systemd/system/portfolio-site-$LAST_DEPLOYED_COMMIT.service
+    systemctl disable $REPO_NAME-$LAST_DEPLOYED_COMMIT
+    systemctl stop $REPO_NAME-$LAST_DEPLOYED_COMMIT
+    rm $SYSTEMD_CONFIG_FOLDER/$REPO_NAME-$LAST_DEPLOYED_COMMIT.service
 
-    rm $RELEASE_FOLDER/portfolio-site-$LAST_DEPLOYED_COMMIT
+    rm $RELEASE_FOLDER/$REPO_NAME-$LAST_DEPLOYED_COMMIT
 }
 
 get_last_deployed_commit
@@ -124,7 +114,6 @@ get_latest_commit
 
 if [[ "$LATEST_COMMIT" != "$LAST_DEPLOYED_COMMIT" ]]; then
     echo "A new change has been pushed"
-    pull_latest_commit
 
     build_stage
     deploy_stage
